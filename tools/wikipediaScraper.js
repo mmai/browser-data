0ae03685138a6613f3c423310b0c0e45e7f80d6c
Henri Bourcereau <henri.bourcereau@gmail.com>
2016-05-04 15:11:33 +0200

    grammar and rules table

--- tools/wikipediaScraper.js
+++ tools/wikipediaScraper.js
@@ -1,18 +1,27 @@
-var jsdom = require("jsdom");
+var jsdom = require('jsdom')
 var fs = require('fs')
 
 var associatedProperties = require('./associatedProperties')
 
 var outputFile = './engineSupportDb.json'
- 
+
 jsdom.env(
   // 'https://en.wikipedia.org/w/index.php?title=Comparison_of_layout_engines_%28Cascading_Style_Sheets%29&printable=yes',
   './tools/Comparison_of_layout_engines.html',
-  ["http://code.jquery.com/jquery.js"],
+  ['http://code.jquery.com/jquery.js'],
   function (err, window) {
-    var tableElem = window.$("#Properties").parent().next().get()[0]
+    var db = {}
+
+    // Properties
+    var tableElem = window.$('#Properties').parent().next().get()[0]
     var tableElements = normalizeTableContents(tableElem)
-    var db = makeJsonFromTableElements(tableElements)
+    db = makeJsonFromTableElements(db, tableElements)
+
+    // Grammar and rules
+    tableElem = window.$('#Grammar_and_rules').parent().next().get()[0]
+    tableElements = normalizeTableContents(tableElem)
+    db = makeJsonFromTableElements(db, tableElements)
+
     fs.writeFile(outputFile, JSON.stringify(db))
   }
 )
@@ -20,55 +29,52 @@ jsdom.env(
 /**
  * makeJsonFromTableElements
  *
- * @param t {Array}
+ * @param table {Array}
  * @return {string}
  */
-function makeJsonFromTableElements(t){
-  var propertyColumnIdx = 1 //properties are on the second column of the table
-  var db = {}
+function makeJsonFromTableElements (db, table) {
+  var propertyColumnIdx = 1 // properties are on the second column of the table
 
-  //Get browser engines
+  // Get browser engines
   var engines = {}
-  t.shift().map(function(cell, i) {
-      var name = cell.textContent.trim()
-      if (name !== "") engines[i] = name
-    })
+  table.shift().map(function (cell, i) {
+    var name = cell.textContent.trim()
+    if (name !== '') engines[i] = name
+  })
 
-  //Get properties support
-  t.map(row => {
-      var td = row[propertyColumnIdx]
-      if (td) {
-        var propertyElem = td.firstChild
-        if (propertyElem && propertyElem.tagName === "CODE") {
-          var property = propertyElem.textContent
+  // Get properties support
+  table.map(row => {
+    var td = row[propertyColumnIdx]
+    if (td) {
+      var propertyElem = td.firstChild
+      if (propertyElem && propertyElem.tagName === 'CODE') {
+        var property = propertyElem.textContent
 
-          Object.keys(engines).map(function(pos){
-              var engine = engines[pos]
-              var support = parseSupport(row[pos].textContent)
-              addPropertiesSupport(db, engine, getAssociatedProperties(property), support)
-            })
-
-        }
+        Object.keys(engines).map(function (pos) {
+          var engine = engines[pos]
+          var support = parseSupport(row[pos].textContent)
+          addPropertiesSupport(db, engine, getAssociatedProperties(property), support)
+        })
       }
-   })
+    }
+  })
   return db
 }
 
-function addPropertiesSupport(db, engine, properties, support){
-  properties.map(function(property){
-      if (!db.hasOwnProperty(property)) db[property] = {}
-      db[property][engine] = support
-    })
+function addPropertiesSupport (db, engine, properties, support) {
+  properties.map(function (property) {
+    if (!db.hasOwnProperty(property)) db[property] = {}
+    db[property][engine] = support
+  })
 }
 
-function getAssociatedProperties(property){
-  return associatedProperties[property] || [property] 
+function getAssociatedProperties (property) {
+  return associatedProperties[property] || [property]
 }
 
-function parseSupport(txt){
-  return txt.replace(/\[[^\]]*\]/, "").trim()
+function parseSupport (txt) {
+  return txt.replace(/\[[^\]]*\]/, '').trim()
 }
-
 
 /**
  * normalizeTableContents
@@ -78,27 +84,26 @@ function parseSupport(txt){
  * @param table {DOMElement}
  * @return {Array} Array of Arrays of DOMElements
  */
-function normalizeTableContents(table) {
+function normalizeTableContents (table) {
   var width = Array.from(table.rows[0].cells).reduce(function (a, v) {
-      return a + parseInt(v.colSpan, 10);
-  }, 0);
+    return a + parseInt(v.colSpan, 10)
+  }, 0)
   var M = Array.from(Array(table.rows.length), function (_) {
-    return Array(width).fill(null);
-  });
+    return Array(width).fill(null)
+  })
   for (var i = 0, len = table.rows.length; i < len; i++) {
-    var tr = table.rows[i];
+    var tr = table.rows[i]
     for (var j = 0, k = 0, jlen = tr.cells.length; j < jlen; j++) {
-      var td = tr.cells[j];
-      k = M[i].indexOf(null, k);
+      var td = tr.cells[j]
+      k = M[i].indexOf(null, k)
       for (var r = 0; r < parseInt(td.rowSpan); r++) {
-        var _M;
+        var _M
 
-        (_M = M[i + r]).splice.apply(_M, [k, parseInt(td.colSpan)].concat(_toConsumableArray(Array(parseInt(td.colSpan)).fill(td))));
+        ;(_M = M[i + r]).splice.apply(_M, [k, parseInt(td.colSpan)].concat(_toConsumableArray(Array(parseInt(td.colSpan)).fill(td))))
       }
     }
   }
-  return M;
+  return M
 }
 
-function _toConsumableArray(arr) { if (Array.isArray(arr)) { for (var i = 0, arr2 = Array(arr.length); i < arr.length; i++) { arr2[i] = arr[i]; } return arr2; } else { return Array.from(arr); } }
-
+function _toConsumableArray (arr) { if (Array.isArray(arr)) { for (var i = 0, arr2 = Array(arr.length); i < arr.length; i++) { arr2[i] = arr[i]; } return arr2; } else { return Array.from(arr); } }
